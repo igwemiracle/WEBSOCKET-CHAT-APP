fcc2656e58df11571f07995759014ff9c9d77c49
igwemiracle <igwemiracle35@gmail.com>
2023-09-05 10:50:26 +0100

    fastapi-websocket

--- fastapi-websocket-chat/Models/sqlData.py
+++ fastapi-websocket-chat/Models/sqlData.py
@@ -4,8 +4,16 @@ Base = declarative_base()
 
 
 class User(Base):
-    __tablename__ = "websocket"
+    __tablename__ = "users"
     id = sa.Column(sa.Integer, primary_key=True, index=True)
     username = sa.Column(sa.String)
     hash_password = sa.Column(sa.String)
     email = sa.Column(sa.String, unique=True, index=True)
+
+
+class SavedMessage(Base):
+    __tablename__ = "messages"
+    id = sa.Column(sa.Integer, primary_key=True)
+    sender_username = sa.Column(sa.String)
+    recipient_username = sa.Column(sa.String)
+    text = sa.Column(sa.String)
